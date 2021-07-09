21fb12af9b2d0e9ef11510e65170153fb219ae43
Takuto Yanagida <yanagida@stxst.com>
2021-07-09 14:08:50 +0900

    Fix gulp

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -3,7 +3,7 @@
  * Gulpfile
  *
  * @author Takuto Yanagida
- * @version 2021-07-08
+ * @version 2021-07-09
  *
  */
 
@@ -12,7 +12,7 @@
 
 const gulp = require('gulp');
 
-const { makeJsTask, makeCssTask, makeCopyTask } = require('./common');
+const { makeJsTask, makeCssTask, makeSassTask, makeCopyTask } = require('./common');
 
 const js_raw = makeJsTask(['src/**/*.js', '!src/**/*.min.js'], './dist', 'src');
 
@@ -26,14 +26,17 @@ const css_copy = makeCopyTask('src/**/*.min.css', './dist');
 
 const css = gulp.parallel(css_raw, css_copy);
 
+const sass = makeSassTask('src/**/*.scss', './dist');
+
 const php = makeCopyTask('src/**/*.php', './dist');
 
 const watch = (done) => {
 	gulp.watch('src/**/*.js', js);
 	gulp.watch('src/**/*.css', css);
+	gulp.watch('src/**/*.scss', sass);
 	gulp.watch('src/**/*.php', php);
 	done();
 };
 
-exports.build   = gulp.parallel(js, css, php);
+exports.build   = gulp.parallel(js, css, sass, php);
 exports.default = gulp.series(exports.build, watch);
