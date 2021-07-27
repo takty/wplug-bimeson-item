d535c0e838f50d3cb791391291b29d3da6831aae
Takuto Yanagida <yanagida@stxst.com>
2021-07-27 10:53:39 +0900

    Change text label

--- sample/page_publication_admin.php
+++ sample/page_publication_admin.php
@@ -4,13 +4,13 @@
  * Admin for the Template for Publication Static Pages
  *
  * @author Takuto Yanagida
- * @version 2021-07-20
+ * @version 2021-07-27
  *
  */
 
 
 function setup_template_admin() {
-	\wplug\bimeson_item\add_meta_box( __( 'Publication list' ), 'page' );
+	\wplug\bimeson_item\add_meta_box( __( 'Publication List' ), 'page' );
 
 	add_action( 'save_post_page', function ( $post_id ) {
 		if ( ! current_user_can( 'edit_post', $post_id ) ) return;
